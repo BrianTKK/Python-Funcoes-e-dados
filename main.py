5db9af53a10dac491838077853c14add37f321e2
Brian <brianhenriquedp@gmail.com>
2025-05-16 18:24:45 -0400

    feat(7): Adiciona funcionalidades de leitura e escrita no README
    Técnicas utilizadas:
    - Abertura de arquivo com open() nos modos 'r' e 'a'.
    - Especificação de encoding="utf-8".
    - Leitura do conteúdo com .read().
    - Obtenção de input do usuário com input().
    - Escrita no arquivo com .write().
    - Fechamento explícito do arquivo com .close().
    - Definição e chamada de funções.

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-arquivo_tarefas = open("readme.md", mode="r", encoding="utf-8")
+def listar_readme():
+    arquivo_readme = open("readme.md", "r", encoding="utf-8")
+    print("readme: ")
+    print(arquivo_readme.read())
 
-print(arquivo_tarefas.read())
+
+def escrever_readme():
+    texto = input("O que deseja adicionar ao readme? ")
+    arquivo_readme = open("readme.md", "a", encoding="utf-8")
+    arquivo_readme.write("\n\n" + texto)
+    arquivo_readme.close()
+
+listar_readme()
+escrever_readme()
+listar_readme()
