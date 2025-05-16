1460f12e7d7f822ff7c2252e717d9deeb65ed42a
Brian <brianhenriquedp@gmail.com>
2025-05-16 15:11:25 -0400

    feat(5): Adiciona exemplo explorando métodos de string
    Inclui um snippet de código Python para demonstrar o uso dos seguintes métodos de string:
    - .upper()
    - .lower()
    - .split()
    - .replace()
    - .find()

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-nome = "Manoela"
+nome = "Manuela"
 
-print(len(nome))
+# upper, lower, split, replace, find
 
-print(nome[0:4])
+print(nome.upper())  # Converte para maiúsculas
+print(nome.lower())  # Converte para minúsculas
+print(nome.split('u'))  # Divide a string em uma lista
+print(nome.replace('ela', '!'))  # Substitui 'ela' por '!'
+print(nome.find('a'))  # Encontra a posição do primeiro 'a'
