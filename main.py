9640d95b52064f4ca62b6f4d0f6a30c81aba1121
Brian <brianhenriquedp@gmail.com>
2025-05-16 19:16:56 -0400

    feat(7.1): Adiciona tratamento de erro para operações de arquivo
    Implementa blocos try/except para capturar FileNotFoundError e outras exceções durante a leitura e escrita do readme.md. Garante uma mensagem de erro informativa e saída controlada usando sys.exit() em caso de falha.

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
-def listar_readme():
-    arquivo_readme = open("readme.md", "r", encoding="utf-8")
-    print("readme: ")
-    print(arquivo_readme.read())
+import sys
 
+try:
+    def listar_readme():
+        arquivo_readme = open("readme.md", "r", encoding="utf-8")
+        print("readme: ")
+        print(arquivo_readme.read())
 
-def escrever_readme():
-    texto = input("O que deseja adicionar ao readme? ")
-    arquivo_readme = open("readme.md", "a", encoding="utf-8")
-    arquivo_readme.write("\n\n" + texto)
-    arquivo_readme.close()
+    def escrever_readme():
+        texto = input("O que deseja adicionar ao readme? ")
+        arquivo_readme = open("readme.md", "a", encoding="utf-8")
+        arquivo_readme.write("\n\n" + texto)
+        arquivo_readme.close()
 
-listar_readme()
-escrever_readme()
-listar_readme()
+    listar_readme()
+    escrever_readme()
+    listar_readme()
+except FileNotFoundError:
+    print("Crie o arquivo readme.md para usar o programa.")
+    sys.exit(1)
+except Exception as e:
+    print(f"Ocorreu um erro: {e}")
+    sys.exit(1)
